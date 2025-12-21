1c8fa3e514eae5b4663179954aa933cc66f8f99b
kishansinghifs1 <kishansingh956196@gmai.com>
2025-12-21 12:48:21 +0530

    feat: initialize problem service with API endpoints, data models, and business logic.

---
+++ Problem-Service/src/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { PORT } = require('./config/server.config');
+const apiRouter = require('./routes');
+const errorHandler = require('./utils/errorHandler');
+const connectToDB = require('./config/db.config');
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+
+//all the request that starts with /api will be mapped to apiRouter
+app.use('/api', apiRouter);
+
+
+app.get('/ping', (req, res) => {
+    return res.json({message: 'Problem Service is alive'});
+});
+
+//global catches for the error handler 
+app.use(errorHandler);
+
+app.listen(PORT, async () => {
+    console.log(`Server started at PORT: ${PORT}`);
+    await connectToDB();
+    console.log("Successfully connected to db");
+});
